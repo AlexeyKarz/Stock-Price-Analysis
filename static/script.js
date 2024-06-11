9effde7b6ffbf33c2769bcd9d9c4f899ac2c53ea
alexeyks <karzanovalexey@gmail.com>
2024-06-11 21:06:34 +0300

    add disclaimer

--- static/script.js
+++ static/script.js
@@ -26,30 +26,3 @@ function findSymbol() {
 function setSymbol(symbol) {
     document.getElementById('stock-name').value = symbol;
 }
-
-// function displayStockOverview(symbol) {
-//     fetch(`/stock_overview/${symbol}`)  // Adjusted to the correct endpoint
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.error) {
-//                 document.getElementById('stock-overview').innerHTML = data.error;
-//             } else {
-//                 // Assuming all keys are properly named and the data structure is as expected
-//                 const info = `
-//                     <h3>Stock Overview for ${data.Symbol}</h3>
-//                     <p><strong>Company:</strong> ${data.Name}</p>
-//                     <p><strong>Sector:</strong> ${data.Sector}</p>
-//                     <p><strong>Industry:</strong> ${data.Industry}</p>
-//                     <p><strong>Description:</strong> ${data.Description}</p>
-//                     <p><strong>Market Cap:</strong> ${data.MarketCapitalization}</p>
-//                     <p><strong>EBITDA:</strong> ${data.EBITDA}</p>
-//                     <p><strong>PE Ratio:</strong> ${data.PERatio}</p>
-//                     <p><strong>Dividend Yield:</strong> ${data.DividendYield}</p>
-//                     <!-- Add more fields as necessary -->
-//                 `;
-//                 document.getElementById('stock-overview').innerHTML = info;
-//             }
-//         })
-//         .catch(error => console.error('Error fetching stock overview:', error));
-// }
-//
